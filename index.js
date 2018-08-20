6b47c813f21fac62e1190ae35291d3b46282eb71
Viam Mercer <viam.mercer@hotmail.com>
2018-08-20 14:29:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions and code here
+var kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  kittens.push(name);
+}
